21aa0c216f8b7c930e4571a1e3a21092e65c8144
PX-72 <zoli.konyves@pm.me>
2023-03-18 16:40:04 +0000

    keyof, typeof

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_AGE = 12;
+export const DEFAULT_NAME = 'John';
+export const DEFAULT_ACTION = 'create';
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
- ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// literal types
+// https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#literal-types
+// they are types that represent specific, fixed values and can be created using string, number, or boolean literals.
+
+// these are the possible literal types:
+let b: true;
+let n: 512;
+let s: 'blah'
+
+n = 512; 
+n = 234; // error!
+
+// they are mostly useful for expressing 
+let x: 1 | 2 | 3;
+x = 3;
+x = 2;
+x = 4; // error!
+
+let y: 1 & 2; // not possible, so it results in never
+y = 1; // error!
+
+// the boolean type is 
+type bool = true | false; 
  
- // union types
- // see: https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#union-types
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// union types
+// see: https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#union-types
 
 type NumOrStr = number | string;
 
@@ -48,32 +74,58 @@ tu = {}; // error!
  //NOTE: this is valid, but it'll will result in 'never' as there is no intersection between them
  type NumAndString = number & string; // = never 
 
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// literal type - a literal type is a type that represents a specific, fixed value. 
-// Literal types can be created using string, number, or boolean literals.
+// keyof operator
+// https://www.typescriptlang.org/docs/handbook/2/keyof-types.html
 
-// these are the possible literal types:
-let b: true;
-let n: 512;
-let s: 'blah'
+// It takes an object type and produces a string literal union of its keys.
+type Point = { x: number; y: number };
+type P = keyof Point; // P is 'x' | 'y'
 
-n = 512; 
-n = 234; // error!
+// use cases:
 
-let x: 1 | 2 | 3;
-x = 3;
-x = 2;
-x = 4; // error!
+// 1. pick out a property from an array of objects
+const pluck = <T, Key extends keyof T>(items: T[], key: Key): T[Key][] => items.map(item => item[key]);
+const namesOnly = pluck([{name: 'a', age: 2}, {name: 'b', age: 3}, {name: 'c', age: 4}], 'name');
+ // ['a', 'b', 'c'];
 
-let y: 1 & 2; // not possible, so it results in never
-y = 1; // error!
-
-// the boolean type is 
-type bool = true | false;
+ // 2. DeepPartial
+ type DeepPartial<T> = Partial<{ [P in keyof T]: DeepPartial<T[P]> }>;
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
- 
 
+// typeof operator
+// https://www.typescriptlang.org/docs/handbook/2/typeof-types.html
+
+// This is the same as the javascript typeof operator
+let greeting = 'hello';
+let str: typeof greeting; // str type is string
+str = 23; // error! (number is not assignable to string)
+
+// use cases:
+
+// 1. ReturType
+const getPerson = (name: string, age: number) => ({ name, age });
+type Person = ReturnType<typeof getPerson>;
+
+// 2. dynamic module import
+type ActionModule =  typeof import('./constants');
+type DefaultKeys = keyof ActionModule;
+type DefaultValues = ActionModule[keyof ActionModule];
+
+// 3. objects
+const someData = {
+    name: 'Bradley',
+    age: 23
+};
+
+type OKeysWrong = keyof someData; // error! 'someData' refers to a value, but is being used as a type here.
+type OKeys = keyof typeof someData; // name | age; (keyof needs a type not a value)
+type OType = typeof someData; // { name: string; age: number; }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 export {};
