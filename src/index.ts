b4c9c87469e7079e19495862bcba1da68ce81db2
PX-72 <zoli.konyves@pm.me>
2023-03-18 11:44:35 +0000

    literal types

--- src/index.ts
+++ src/index.ts
@@ -48,11 +48,29 @@ tu = {}; // error!
  //NOTE: this is valid, but it'll will result in 'never' as there is no intersection between them
  type NumAndString = number & string; // = never 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// literal type - a literal type is a type that represents a specific, fixed value. 
+// Literal types can be created using string, number, or boolean literals.
 
+// these are the possible literal types:
+let b: true;
+let n: 512;
+let s: 'blah'
 
+n = 512; 
+n = 234; // error!
 
+let x: 1 | 2 | 3;
+x = 3;
+x = 2;
+x = 4; // error!
 
+let y: 1 & 2; // not possible, so it results in never
+y = 1; // error!
+
+// the boolean type is 
+type bool = true | false;
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
