4d344966d830dfd0a2d4a40c62423b4ff0f99262
PX-72 <zoli.konyves@pm.me>
2023-03-18 11:11:03 +0000

    intersection

--- src/index.ts
+++ src/index.ts
@@ -14,28 +14,39 @@ ns = []; // error!
 type T1 = { name: string, colour: string };
 type T2 = { age: number };
 
-type T3 = T1 | T2;
+type TUnion = T1 | T2;
 
 // union type rule is satisfied by one of the types fully present
-let t3: T3 = {
+let tu: TUnion = {
     name: 'a name',
     age: 23
 };
 
 // neither type is fully present
-t3 = {
-    name: 'b name'
-};
+tu = { name: 'b name' }; // error!
 
-t3 = {}; // error!
+tu = {}; // error!
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
- // intersection types
- // see: https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#union-types
+ type TIntersection = T1 & T2;
+
+ // intersection type must satisfy both T1 and T2 - effectively same as extending an interface
+ let ti: TIntersection = {
+     name: 'a name',
+     colour: 'red',
+     age: 23
+ };
+ 
+ // only one type is satisfied (colour is missing from T1)
+ ti = {
+     name: 'b name',
+     age: 23
+ };
 
 
-
+ //NOTE: this is valid, but it'll will result in 'never' as there is no intersection between them
+ type NumAndString = number & string; // = never 
 
 
 
